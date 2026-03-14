be3e6f067cb86faac20d46cd7616db951fef25aa
Rafael Alves <joao.alves079@academico.ifs.edu.br>
2026-03-14 13:56:54 -0300

    change tsconfig due to import errors

--- rest_escola__start_point/escola-api/src/db.ts
+++ rest_escola__start_point/escola-api/src/db.ts
@@ -1,10 +1,9 @@
 import { Pool } from 'pg';
-import 'dotenv/config';
 
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: String(process.env.DB_USER),
+    host: String(process.env.DB_HOST),
+    database: String(process.env.DB_NAME),
+    password: String(process.env.DB_PASSWORD),
     port: parseInt(process.env.DB_PORT || '5432', 10),
 });
